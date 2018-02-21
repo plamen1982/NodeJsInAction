8a79d697ef4f04959e0f59870908a2d0ae92555a
“[plamen1982]” <plamen.hristov@ymail.com>
2018-02-21 13:09:49 -0500

    Part2:Ch4, Middleware, 404 custom middleware, fix sendFile to send

--- part2/staticWegServer/app.js
+++ part2/staticWegServer/app.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 // 404 is always ot the bottom of all middlewares no matter what.
 app.use((req, res) => {
     res.status(404);
-    res.sendFile("File not found");
+    res.send("File not found");
 })
 
 app.listen(PORT, () => {
