e19d55699c1bab212c066e15d7d169c5561d7e61
“[plamen1982]” <plamen.hristov@ymail.com>
2018-02-21 14:44:51 -0500

    Switching to express.static middleware

--- part2/staticWegServer/app.js
+++ part2/staticWegServer/app.js
@@ -1,33 +1,20 @@
 const express = require('express');
 const logger = require('morgan');
 const path = require('path');
-const fs = require('fs');
 
 const PORT = 3000;
-
+let staticPath = path.join(__dirname, "static");
 let app = express();
 
 app.use(logger("short"));
 
-app.use((req, res, next) => {
-    let filePath = path.join(__dirname, "static", req.url);
-    fs.stat(filePath, (err, fileInfo) => {
-        if(err) {
-            next();
-            return;
-        }
-        if(fileInfo.isFile()) {
-            res.sendFile(filePath);
-        } else {
-            next();
-        }
-    })
-})
+app.use(express.static(staticPath));
+
 // 404 is always ot the bottom of all middlewares no matter what.
 app.use((req, res) => {
     res.status(404);
     res.send("File not found");
-})
+});
 
 app.listen(PORT, () => {
     console.log(`App started on port: ${PORT}`)
