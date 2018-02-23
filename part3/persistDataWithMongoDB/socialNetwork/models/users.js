e3a3e23ae206740b7a81658da34497ae81c50979
“[plamen1982]” <plamen.hristov@ymail.com>
2018-02-23 13:35:37 -0500

    Created users schema

---
+++ part3/persistDataWithMongoDB/socialNetwork/models/users.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const userSchema = mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+    displayName: String,
+    bio: String
+});
