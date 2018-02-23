3bf805e1f3f38159b72751a8829317ef3fc56255
“[plamen1982]” <plamen.hristov@ymail.com>
2018-02-23 13:53:08 -0500

    Pre-save action to hash the password

--- part3/persistDataWithMongoDB/socialNetwork/models/users.js
+++ part3/persistDataWithMongoDB/socialNetwork/models/users.js
@@ -1,4 +1,8 @@
+const bcrypt = require('bcrypt-nodejs');
 const mongoose = require('mongoose');
+
+const SALT_FACTOR = 10;
+
 const userSchema = mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
@@ -10,3 +14,25 @@ const userSchema = mongoose.Schema({
 userSchema.methods.name = () => {
     return this.displayName || this.username
 };
+//do nothing function fot use with bcrypt module
+let noop = () => {};
+
+//runs before model is saved
+userSchema.pre('save', (done)=>{
+    let user = this;
+
+//skips if password is not modified
+    if(!user.isModified("password")) {
+        return done();
+    }
+//generate a salt for the hash, and calls the inner function to completed
+    bcrypt.genSalt(SALT_FACTOR, (err, salt) => {
+        if(err) { return done(err); }
+        bcrypt.hash(user.password, salt, noop, (err, hashedPassword) => {
+            if(err) { return done(); }
+//stores the password and continue with saving
+            user.password = hashedPassword;
+            done(); 
+        });
+    });
+});
