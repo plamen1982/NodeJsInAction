b99fc3f1c216c6e266657b0efe756043847c6c7e
“[plamen1982]” <plamen.hristov@ymail.com>
2018-03-02 11:00:52 -0500

    Creating Ecommerce project for ordering food

---
+++ projectEcommercePlace/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const flash = require('connect-flash');
+const passport = require('passport');
+
